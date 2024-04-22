7b5d1c60694b9a4cbe3d1b61feeda0eded6ab3a3
jayavardhanraop <jaya123vardhan@gmail.com>
2024-04-22 10:53:18 +0530

    adding same function from main branch

--- file_1.c
+++ file_1.c
@@ -4,7 +4,7 @@ int main() {
 	int number1, number2, sum;
 
 	printf("Enter two integers: ");
-	scanf(" %d %d, &number1, &number2);
+	scanf(" %d %d", &number1, &number2);
 
 	//calculate the sum
 	sum = number1 + number2;
@@ -20,3 +20,9 @@ int addfunc( int a, int b)
 	int c = a+b;
 	return c;
 =========================================
+//function added in branch1
+int func_add(int a, int b)
+{
+	return a+b;
+>>>>>> branch1
+}
